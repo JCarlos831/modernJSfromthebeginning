79f8d22c3205e2e130359fb1c5b5ea8f0773508c
Juan Montoya <jcmontoya@gmail.com>
2018-02-21 20:20:01 +0000

    Section 2 Lecture 9 - Numbers and the Math Object

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,45 +1,28 @@
-//TYPE CONVERSION
+// Numbers and the Math Object
 
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string
-val = String(555);
-val = String(4+4);
-// Boolean to a string
-val = String(true);
-// Date to a string
-val = String(new Date());
-// Array to a string
-val = String([1,2,3,4]);
+// Simple Math with Numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// toString()
-val = (5).toString();
-val = (true).toString();
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.floor(Math.random() * 20 + 1);
 
-// String to Number
-val = Number('5');
-// Boolean to Number
-val = Number(true); // 1
-val = Number(false); // 0
-val = Number(null); // 0
-val = Number('hello'); // NaN (Not a Number)
-val = Number([1,2,3]);  // Nan (Not a Number)
-
-val = parseInt('100.30');
-val = parseFloat('100.30');
-
-
-// Output
-// console.log(val);
-// console.log(typeof val);
-// // console.log(val.length);
-// console.log(val.toFixed(2));
-
-//Type Coersion
-
-const val1 = String('5');
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
+console.log(val);
