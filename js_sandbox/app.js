28bdc23b8cd91d14f119a6e2e39a2fd045b04189
Juan Montoya <jcmontoya@gmail.com>
2018-02-21 16:42:50 +0000

    Section 2 Lecture 5

---
+++ js_sandbox/app.js
@@ -0,0 +1,28 @@
+// Log to console
+console.log('Hello World');  //String
+console.log(123);  //Numbers
+console.log(true);  //Boolean
+var greeting = 'Hello'; 
+console.log(greeting);  //Variables
+console.log([1,2,3,4]);  //Arrays
+console.log({a:1, b:2});  //Objects
+console.table({a:1, b:2});  //Objects appear as a table in the console.
+
+console.error('This is some error');  //Will appear in red in the console
+console.clear();  //Will clear the console
+console.warn('This is a warning');  //Will appear in yellow in the console.
+
+console.time('Hello');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+console.timeEnd('Hello');
+/*
+    multi
+    line
+    comments
+*/
+    
