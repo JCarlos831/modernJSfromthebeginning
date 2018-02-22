2714bf76ca3ac17b846a46289dfae7ac94db1291
Juan Montoya <jcmontoya@gmail.com>
2018-02-22 16:17:03 +0000

    SECTION 2 LECTURE 16 - SWITCHES

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,102 +1,43 @@
-//SECTION 2 LECTURE 15 - IF STATEMENTS & COMPARISON OPERATORS
-
-// if(something){
-//     do something;
-// } else {
-//     do something else;
-// }
-
-const id = 100;
-
-// // Equal to
-
-// if (id == 100) {
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// // Note equal to
-
-// if (id != 101) {
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// // Equal to value and type
-
-// if (id === 100) {
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// // Not equal to value and type
-
-// if (id !== 100) {
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-
-// // Test if undefined
-// if(typeof id !== 'undefined') {
-//     console.log(`The ID is ${id}`);
-// } else {
-//     console.log('NO ID');
-// }
-
-// Greater or less than
-
-// if (id >= 100) {
-//     console.log('CORRECT');
-// } else {
-//     console.log('INCORRECT');
-// }
-
-// IF ELSE
+//SECTION 2 LECTURE 16 - SWITCHES
 
 const color = 'yellow';
 
-// if(color === 'red') {
-//     console.log('Color is red');
-// } else if (color === 'blue') {
-//     console.log('Color is blue');
-// } else {
-//     console.log('Color is not red or blue');
-// }
-
-// Logical Operators
-
-const name = 'Steve';
-const age = 25;
-
-// And &&
-
-if(age > 0 && age <= 12){
-    console.log(`${name} is a child`)
-} else if (age >= 13 && age <= 19) {
-    console.log(`${name} is a teenager`);
-} else {
-    console.log(`${name} is an adult`);
+switch(color) {
+    case 'red' : 
+        console.log('Color is red');
+        break;
+    case 'blue' :
+        console.log('Color is blue');
+        break;
+    default :
+        console.log('Color is not red or blue');
+        break;
 }
 
-// Or ||
+let day;
 
-if (age < 16 || age > 65) {
-    console.log(`${name} can not run in race`);
-} else {
-    console.log(`${name} is registered for the race`);
+switch(new Date().getDay()){
+    case 0 :
+        day = 'Sunday';
+        break;
+    case 1 :
+        day = 'Monday';
+        break;
+    case 2 :
+        day = 'Tuesday';
+        break;
+    case 3 :
+        day = 'Wednesday';
+        break;
+    case 4 :
+        day = 'Thursday';
+        break;
+    case 5 :
+        day = 'Friday';
+        break;
+    case 6 :
+        day = 'Saturday';
+        break;
 }
 
-// Tenary Operator
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
-
-// Without Braces
-
-if(id === 100)
-    console.log('CORRECT');
-else
-    console.log('INCORRECT');
+console.log(`Today is ${day}`);
