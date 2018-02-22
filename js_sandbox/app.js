d363c51854bb6eceae644f09ed2294c404cd9520
Juan Montoya <jcmontoya@gmail.com>
2018-02-22 17:43:51 +0000

    SECTION 2 LECTURE 18 - GENERAL LOOPS & ITERATION

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,53 +1,87 @@
-//SECTION 2 LECTURE 17 - FUNCTION DECLARATIONS & EXPRESSIONS
+//SECTION 2 LECTURE 18 - GENERAL LOOPS & ITERATION
 
-// Function declarations
+// For Loop
 
-function greet(firstName = 'John', lastName = 'Doe') {
-    // if(typeof firstName === 'undefined') {
-    //     firstName = 'John';
-    // }
-    // if(typeof lastName === 'undefined') {
-    //     lastName = 'Doe';
-    // }
-//   console.log('Hello');
-    return 'Hello ' + firstName + ' ' + lastName;
+// for(let i = 0; i < 10; i++){
+//     if(i === 2){
+//         console.log('2 is my favorite number');
+//         continue;
+//     }
+    
+//     if(i === 5){
+//         console.log('Stop the loop');
+//         break;
+//     }
+//     console.log('Number ' + i);
+// }
+
+// While Loop
+
+// let i = 0;
+
+// while(i < 10) {
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+// Do While Loop  Will run once no matter what
+
+// let i = 100;
+
+// do {
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+// while (i < 10);
+
+// Loop Through Arrays
+
+// For Loop
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for(let i = 0; i < cars.length; i++){
+//     console.log(cars[i]);
+// }
+
+// For Each
+
+// cars.forEach(function(car, index, array){
+//     console.log(`${index} : ${car}`);
+//     console.log(array);
+// });
+
+// Map
+
+// const users = [
+//     {
+//         id : 1,
+//         name : 'John'
+//     },
+//     {
+//         id : 2,
+//         name : 'Sara'
+//     },
+//     {
+//         id : 3,
+//         name : 'Karen'
+//     }
+// ];
+
+// const ids = users.map(function(user){
+//     return user.id;
+// });
+    
+// console.log(ids);
+    
+// For In
+
+const user = {
+  firstName : 'John',
+  lastName : 'Doe',
+  age : 40
+};
+
+for(let x in user){
+    console.log(`${x} : ${user[x]}`);
 }
-
-// console.log(greet());
-
-// Function Expressions
-
-const square = function(x = 3) {
-    return x * x;
-};
-
-// console.log(square());
-
-// Immediately Invokable Function Expressions = IIFEs
-
-// (function() {
-//     console.log('IIFE ran...');
-// })();
-
-// (function(name) {
-//     console.log('Hello ' + name);
-// })('Brad');
-
-// Property Methods
-
-const todo = {
-    add: function() {
-        console.log('Add todo...');
-    },
-    edit: function(id) {
-        console.log('Edit todo ' + id);
-    }
-};
-
-todo.delete = function() {
-    console.log('Delete todo...');
-};
-
-todo.add();
-todo.edit(22);
-todo.delete();
