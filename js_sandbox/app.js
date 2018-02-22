fbbf33959f9fc2950eb2dd4346a576698a59bd7c
Juan Montoya <jcmontoya@gmail.com>
2018-02-22 16:33:49 +0000

    SECTION 2 LECTURE 17 - FUNCTION DECLARATIONS & EXPRESSIONS

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,43 +1,53 @@
-//SECTION 2 LECTURE 16 - SWITCHES
+//SECTION 2 LECTURE 17 - FUNCTION DECLARATIONS & EXPRESSIONS
 
-const color = 'yellow';
+// Function declarations
 
-switch(color) {
-    case 'red' : 
-        console.log('Color is red');
-        break;
-    case 'blue' :
-        console.log('Color is blue');
-        break;
-    default :
-        console.log('Color is not red or blue');
-        break;
+function greet(firstName = 'John', lastName = 'Doe') {
+    // if(typeof firstName === 'undefined') {
+    //     firstName = 'John';
+    // }
+    // if(typeof lastName === 'undefined') {
+    //     lastName = 'Doe';
+    // }
+//   console.log('Hello');
+    return 'Hello ' + firstName + ' ' + lastName;
 }
 
-let day;
+// console.log(greet());
 
-switch(new Date().getDay()){
-    case 0 :
-        day = 'Sunday';
-        break;
-    case 1 :
-        day = 'Monday';
-        break;
-    case 2 :
-        day = 'Tuesday';
-        break;
-    case 3 :
-        day = 'Wednesday';
-        break;
-    case 4 :
-        day = 'Thursday';
-        break;
-    case 5 :
-        day = 'Friday';
-        break;
-    case 6 :
-        day = 'Saturday';
-        break;
-}
+// Function Expressions
 
-console.log(`Today is ${day}`);
+const square = function(x = 3) {
+    return x * x;
+};
+
+// console.log(square());
+
+// Immediately Invokable Function Expressions = IIFEs
+
+// (function() {
+//     console.log('IIFE ran...');
+// })();
+
+// (function(name) {
+//     console.log('Hello ' + name);
+// })('Brad');
+
+// Property Methods
+
+const todo = {
+    add: function() {
+        console.log('Add todo...');
+    },
+    edit: function(id) {
+        console.log('Edit todo ' + id);
+    }
+};
+
+todo.delete = function() {
+    console.log('Delete todo...');
+};
+
+todo.add();
+todo.edit(22);
+todo.delete();
