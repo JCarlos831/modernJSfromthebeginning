ffae4c00ae925951996187bebc99f0459a6e4db9
Juan Montoya <jcmontoya@gmail.com>
2018-02-22 15:06:27 +0000

    Section 2 Lecture 12 - Arrays & Array Methods

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,38 +1,68 @@
-// TEMPLATE LITERALS
+// Arrays & Array Methods
 
-const name = 'John';
-const age = 31;
-const job = 'Web Developer';
-const city = 'Miami';
-let html;
+// Create some arrays
+const numbers = [43,56,33,23,44,36,5];
+const numbers2 = new Array(22,45,33,76,54);
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 
-// Without Template Strings/Literals (es5)
+let val;
 
-html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li><ul>';
+// Get array length
+val = numbers.length;
 
-html = '<ul>' +
-        '<li>Name: ' + name + '</li>' +
-        '<li>Age: ' + age + '</li>' +
-        '<li>Job: ' + job + '</li>' +
-        '<li>City: ' + city + '</li>'+
-        '</ul>';
-        
-function hello(){
-    return 'hello';
-}
-        
-// With Template Strings/Literals
+// Check if is array
+val = Array.isArray(numbers);
 
-html = `
-        <ul>
-            <li>Name: ${name}</li>
-            <li>Age: ${age}</li>
-            <li>Job: ${job}</li>
-            <li>City: ${city}</li>
-            <li>${2 + 2}</li>
-            <li>${hello()}</li>
-            <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
-        </ul>
-        `;
+// Get a single value
+val = numbers[3];
+val = numbers[0];
 
-document.body.innerHTML = html;
+// Insert into array
+numbers[2] = 100;
+
+// Find index of value
+val = numbers.indexOf(36);
+
+// MUTATING ARRAYS
+// Add on to end
+// numbers.push(250);
+// // Add on to front
+// numbers.unshift(120);
+// // Take off from end
+// numbers.pop();
+// // Take off from front
+// numbers.shift();
+// // Splice Values
+// numbers.splice(1, 3);
+// // REVERSE
+// numbers.reverse();
+
+
+// CONCATENATE ARRAYS
+val = numbers.concat(numbers2);
+
+// SORT
+val = fruit.sort();
+val = numbers.sort();
+
+// USE THE COMPARE FUNCTION
+val = numbers.sort(function(x,y) {
+    return x-y;
+});
+
+// REVERSE SORT
+val = numbers.sort(function(x,y){
+    return y-x;
+});
+
+// FIND
+
+function under50(num){
+    return num < 50;
+};
+
+val = numbers.find(under50);
+
+console.log(numbers);
+console.log(val);
