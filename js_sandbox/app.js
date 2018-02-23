3200b8bf8269b59a301e37bee77d0556da24fa83
Juan Montoya <jcmontoya@gmail.com>
2018-02-23 16:26:09 +0000

    SECTION 3 LECTURE 23 - DOM SELECTORS FOR SINGLE ELEMENTS

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,42 +1,39 @@
-//SECTION 3 LECTURE 22 - Examining the Document Object
+//SECTION 3 LECTURE 23 - DOM SELECTORS FOR SINGLE ELEMENTS
 
-let val;
+// document.getElementByID()
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// console.log(document.getElementById('task-title'));
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// // Get things from the element
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].className;
-val = document.links[0].classList;
-val = document.links[0].classList[0];
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-val = document.images;
+// const taskTitle = document.getElementById('task-title');
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// // Change styling
 
-let scripts = document.scripts;
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // document.getElementById('task-title').style.display = 'none';
 
-let scriptsArr = Array.from(scripts);
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style = "color:red">Task List</span>';
 
-scriptsArr.forEach(function(script) {
-    console.log(script.getAttribute('src'));
-})
+// document.querySelector();
 
-console.log(val);
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
