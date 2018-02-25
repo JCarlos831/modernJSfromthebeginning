6b20eba3c11ccd064532ccb31ff9ab583a2dcfa4
Juan Montoya <jcmontoya@gmail.com>
2018-02-25 18:23:27 +0000

    Section 4 Lecture 34 - Task List [Part 2] - Delete & Filter Tasks

--- tasklist/app.js
+++ tasklist/app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear Task Event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter Tasks Event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -19,7 +25,7 @@ function addTask(e) {
     if(taskInput.value === '') {
         alert('Add a Task');
     }
-    
+
     // Create li element
     const li = document.createElement('li');
     // Add a class
@@ -42,4 +48,39 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')){
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
+    
+    // Faster
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+    
+    // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
