a43cffa37f207ada14a52575b015808ddcaaf87a
Juan Montoya <jcmontoya@gmail.com>
2018-02-28 21:15:37 +0000

    Section 4 Lecture 41 - Part 3

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -35,16 +35,46 @@ guessBtn.addEventListener('click', function(){
    
    // Check if won
    if(guess === winningNum){
-       // Disable Input
-       guessInput.disabled = true;
-       // Change border to green
-       guessInput.style.borderColor = 'green';
-       // Set message
-       setMessage(`${winningNum} is correct! YOU WIN!`, 'green');
-   } else {
+       // Game over - Won
        
+       gameOver(true, `${winningNum} is correct, YOU WIN!`)
+   } else {
+       // Wrong number
+       guessesLeft -= 1;
+       
+       if(guessesLeft === 0){
+           // Game Over - Lost
+           gameOver(false, `Game Over, you lost. the correct number was ${winningNum}`)
+           
+       } else {
+           //Game continues - answer wrong
+           
+           // Change Border Color
+           guessInput.style.borderColor = 'red';
+           
+           // ClearInput
+           guessInput.value = '';
+           
+           // Tell user its the wrong number
+           setMessage(`${guess} is not correct, ${guessesLeft} guessesLeft`, 'red');
+       }
    }
 })
+
+// Game Over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+    
+    // Disable Input
+       guessInput.disabled = true;
+       // Change border to green
+       guessInput.style.borderColor = color;
+       // Set text color
+       message.style.color = color;
+       // Set message
+       setMessage(msg);
+}
 
 // Set message
 function setMessage(msg, color){
